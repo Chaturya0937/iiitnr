238b0c0c2b7840599c4f356d6e58601980a2f77b
yadalasrihari186 <yadalasrihari186@gmail.com>
2025-11-23 14:50:59 +0530

    Update GenericLabEquipmentPage.dart

--- lib/GenericLabEquipmentPage.dart
+++ lib/GenericLabEquipmentPage.dart
@@ -1,1 +1,35 @@
+// lib/GenericLabEquipmentPage.dart
 
+import 'package:flutter/material.dart';
+// Add any other imports needed for equipment management (e.g., firebase, requests)
+
+class GenericLabEquipmentPage extends StatelessWidget {
+  // This parameter tells the page which lab's data to display/manage
+  final String labName; 
+  final String collectionName;
+
+  const GenericLabEquipmentPage({
+    super.key, 
+    required this.labName, 
+    required this.collectionName
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$labName Equipment"),
+        // The title is now dynamic!
+      ),
+      body: Center(
+        // **!!! ACTION REQUIRED HERE !!!**
+        // You MUST take the equipment listing/requesting logic 
+        // from your old 'Iotequipment.dart' and 'Dnpequipment.dart' files 
+        // and put it here.
+        // Replace 'IOT_EQUIPMENT_COLLECTION' with 'collectionName' 
+        // to make it dynamic.
+        child: Text("Display and Request equipment for: $labName using collection: $collectionName"),
+      ),
+    );
+  }
+}
